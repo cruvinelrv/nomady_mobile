84a1db4a4b85722898f89496dcf96662e8581d52
vmcruvinel <vinicius.machado@popcode.com.br>
2022-07-10 09:56:18 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/nomadypass/nomady_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.nomadypass.nomady_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/landing_page/landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Landing Page'),
+      ),
+    );
+  }
+}
